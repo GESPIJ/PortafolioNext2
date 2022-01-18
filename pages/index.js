181f6fdf8789db1d3883fb2b59adf139caa218b9
GESPIJ <33352233+GESPIJ@users.noreply.github.com>
2022-01-17 23:54:43 -0400

    Corregido lo del viewport

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,15 @@ import Contact from "../src/components/contact/Contact";
 //import "./app.scss";
 import { useState } from "react";
 import Menu from "../src/components/menu/Menu";
+import Head from "next/head";
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="app">
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
